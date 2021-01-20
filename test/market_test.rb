2678ecdde832fc2947b23214f8281f9dd6f98dbd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-20 11:43:05 -0500

    Revise test setup method, complete market it2

--- test/market_test.rb
+++ test/market_test.rb
@@ -4,29 +4,21 @@ require './lib/vendor'
 require './lib/item'
 require './lib/market'
 
-class VendorTest < Minitest::Test
+class MarketTest < Minitest::Test
 
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    @vendor1.stock(@item1, 35)
-    @vendor1.stock(@item2, 7)
-
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @vendor2.stock(@item4, 50)
-    @vendor2.stock(@item3, 25)
-
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
-
     @vendor3 = Vendor.new("Palisade Peach Shack")
-    @vendor3.stock(i@tem1, 65)
   end
 
   def test_it_exists
-    assert_equal Market, @market
+    assert_instance_of Market, @market
   end
 
   def test_it_has_attributes
@@ -36,6 +28,11 @@ class VendorTest < Minitest::Test
   end
 
   def test_it_can_add_vendors_and_list_by_name
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
@@ -45,6 +42,11 @@ class VendorTest < Minitest::Test
   end
 
   def test_vendors_that_sell_items
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
