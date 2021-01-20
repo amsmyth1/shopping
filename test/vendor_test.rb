2a0e62c9d7f773f6ad4f710eba27fcf3b607df72
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-20 11:16:40 -0500

    Complete vendor tests for it1

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,8 +5,14 @@ require './lib/item'
 
 class VendorTest < Minitest::Test
 
-  def test_it_exists
+  def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+
 
     assert_instance_of Vendor, @vendor
   end
@@ -18,10 +24,20 @@ class VendorTest < Minitest::Test
   end
 
   def test_it_can_check_stock_and_add_stock
-    assert_equal 0, @vendor.check_stock(item1)
 
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_equal 0, @vendor.check_stock(@item1)
+
     @vendor.stock(item1, 30)
+    expect = {@item1 => 30}
+
+    assert_equal expect, @vendor.inventory
+    assert_equal 30, @vendor.check_stock(@item1)
+
+    @vendor.stock(item1, 25)
+    @vendor.stock(item2, 12)
+    expect = {@item1 => 55, @item2 => 12}
+
+    assert_equal 55, @vendor.check_stock(@item1)
+    assert_equal expect, @vendor.inventory
   end
 end
