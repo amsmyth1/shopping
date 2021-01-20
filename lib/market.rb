2e7b7c32d173af33219e9645d1b03c49455c4cbc
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-01-20 11:50:01 -0500

    Complete sorted_item_list and tests

--- lib/market.rb
+++ lib/market.rb
@@ -19,6 +19,18 @@ class Market
   def vendors_that_sell(item)
     @vendors.select do |vendor|
       vendor.in_stock?(item)
-    end 
+    end
+  end
+
+  def sorted_item_list
+    sorted_item_list = []
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quant|
+        if quant > 0
+          sorted_item_list << item.name
+        end
+      end
+    end
+    sorted_item_list.uniq.sort
   end
 end
